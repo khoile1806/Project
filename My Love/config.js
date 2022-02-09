0212dca31947c2e330b3518ba28911a41f860784
khoile1806 <khoile632@gmail.com>
2022-02-09 22:31:57 +0700

    Wed project

--- My Love/config.js
+++ My Love/config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: '<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy cái mặt bự ấy ',
     btnYes: 'Vẫn cứ là yêu anh <33',
     btnNo: 'Chỉ thích thôi',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Đợi hết đợt dịch này anh sẽ về dẫn em đi chơi.',
+    reply: 'Hồ Thị Thảo yêu anh Khôi nhất trên đời này <333333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều nhiều nhiều 😘😘',
+    messDesc: 'Anh sẽ dẫn bé đi ngao du khắp thế giang này luôn nhoaaaaa',
     btnAccept: 'Yêu em lắm',
     messLink: 'https://www.facebook.com/profile.php?id=100022633120844' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
